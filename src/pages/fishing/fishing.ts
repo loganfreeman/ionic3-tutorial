e6c4fd956b9919213222838b8bccd4d374f1f7a0
shanhong cheng <shanhong.cheng@utah.edu>
2017-08-25 13:48:54 -0600

    filter

--- src/pages/fishing/fishing.ts
+++ src/pages/fishing/fishing.ts
@@ -17,37 +17,56 @@ import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-na
   templateUrl: 'fishing.html'
 })
 export class FishingPage {
-  icons: string[];
   items: Observable<any>;
+  originalItems: Observable<any>;
+  keyword: string;
   loading: Loading  = this.loadingCtrl.create({
     spinner: 'hide',
     content: 'Loading Please Wait...'
   });
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public apiProvider: ApiNativeProvider, public loadingCtrl: LoadingController, private launchNavigator: LaunchNavigator) {
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
     this.items = this.apiProvider.getHotSpots();
+
+    this.originalItems = this.items;
 
     this.items.subscribe(response=>{
       this.loading.dismiss();
     });
 
     this.presentLoadingText();
-    // this.items = [];
-    // for(let i = 1; i < 11; i++) {
-    //   this.items.push({
-    //     title: 'Item ' + i,
-    //     note: 'This is item #' + i,
-    //     icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-    //   });
-    // }
   }
 
   navigate(latitude, longitude) {
     console.log(latitude, longitude);
     this.launchNavigator.navigate([latitude, longitude]);
+  }
+
+  setFilter() {
+    //console.log(this.keyword);
+    if(this.keyword.trim().length === 0) {
+      return this.items = this.originalItems;
+    }
+    let reg;
+    try {
+      reg = new RegExp(this.keyword, 'i');
+    }catch(e) {
+      return this.items = this.originalItems;
+    }
+    return this.items = this.originalItems
+         .map(items => items.filter(item => {
+           return item.title.match(reg);
+         }));
+  }
+
+  onStatusChange(status) {
+    if(status === 'All') {
+      return this.items = this.originalItems;
+    }
+    return this.items = this.originalItems
+         .map(items => items.filter(item => {
+           return item.status === status;
+         }));
   }
 
   presentLoadingText() {
