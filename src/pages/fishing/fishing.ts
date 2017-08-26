448dcefa6bbbe50b5bb52b5a4695576003a7c2c9
shanhong cheng <shnahongcheng@gmail.com>
2017-08-25 18:45:06 -0600

    geolib

--- src/pages/fishing/fishing.ts
+++ src/pages/fishing/fishing.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
-import { NavController, NavParams, LoadingController, Loading } from 'ionic-angular';
+import { NavController, NavParams, LoadingController, Loading, AlertController } from 'ionic-angular';
 
 import { IonicPage } from 'ionic-angular';
 
@@ -12,6 +12,10 @@ import { LaunchNavigator, LaunchNavigatorOptions } from '@ionic-native/launch-na
 
 import { Geolocation ,GeolocationOptions ,Geoposition ,PositionError } from '@ionic-native/geolocation';
 
+import * as geolib from 'geolib';
+
+declare var google;
+
 @IonicPage()
 @Component({
   selector: 'page-list',
@@ -21,7 +25,7 @@ export class FishingPage {
   options : GeolocationOptions;
   items: Observable<any>;
   originalItems: Observable<any>;
-  currentPos: Observable<Geoposition>;
+  currentPos: Promise<Geoposition>;
   distance: number;
   keyword: string;
   loading: Loading  = this.loadingCtrl.create({
@@ -30,7 +34,7 @@ export class FishingPage {
   });
 
   constructor(public navCtrl: NavController, public navParams: NavParams, public apiProvider: ApiNativeProvider, public loadingCtrl: LoadingController,
-    private launchNavigator: LaunchNavigator, private geolocation : Geolocation) {
+    private launchNavigator: LaunchNavigator, private geolocation : Geolocation, public alertCtrl: AlertController) {
     this.items = this.apiProvider.getHotSpots();
 
     this.originalItems = this.items;
@@ -48,12 +52,22 @@ export class FishingPage {
       this.currentPos = this.getUserPosition();
   }
 
+  showAlert(title, subtitle) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subtitle,
+      buttons: ['OK']
+    });
+    alert.present();
+  }
+
   getUserPosition() {
     this.options = {
         enableHighAccuracy : true
     };
     let promise = this.geolocation.getCurrentPosition(this.options);
-    return Observable.fromPromise(promise);
+    //return Observable.fromPromise(promise);
+    return promise;
   }
 
   navigate(latitude, longitude) {
@@ -82,6 +96,28 @@ export class FishingPage {
     if(status === 'All') {
       return this.items = this.originalItems;
     }
+    return this.items = this.originalItems
+         .map(items => items.filter(item => {
+           return item.status === status;
+         }));
+  }
+
+  distanceChange() {
+    // this.showAlert("distance changed", this.distance);
+    this.currentPos.then(pos => {
+      let distance = geolib.getDistance(
+          {latitude: pos.coords.latitude, longitude: pos.coords.longitude},
+          {latitude: "51° 31' N", longitude: "7° 28' E"}
+      );
+      this.showAlert("distance", distance);
+    });
+  }
+
+  getHotSpotsInRadius(distance, currentPos) {
+    if(!distance) {
+      return this.items = this.originalItems;
+    }
+
     return this.items = this.originalItems
          .map(items => items.filter(item => {
            return item.status === status;
